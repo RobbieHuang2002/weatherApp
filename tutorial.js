82850f0f7496b3ce4816be6923c5abee9789481e
RobbieHuang2002 <you@example.com>
2022-01-03 18:59:33 -0500

    Able to get information from openweather api

--- tutorial.js
+++ tutorial.js
@@ -1,6 +1,53 @@
 const api = {
     key: "0852481dd3c1146e67a097114b826076",
-    baseurl: "api.openweathermap.org/data/2.5"
+    baseurl: "https://api.openweathermap.org/data/2.5/"
 }
 
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
 
+function setQuery(evt){
+    if (evt.keyCode == 13){
+        getResults(searchbox.value);
+    }
+}
+
+function getResults(query){
+    fetch(`${api.baseurl}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then(weather => {
+        return weather.json();
+    }).then(displayResults);
+}
+
+function displayResults(weather){
+    console.log(weather);
+    let city = document.querySelector('.location .city');
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+    let now = new Date();
+    let date = document.querySelector('.location .date');
+    date.innerText = dateBuilder(now);
+
+    let temp = document.querySelector('.current .temp');
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c </span>`;
+
+    let weather_element = document.querySelector('.current .weather');
+    weather_element.innerText = weather.weather[0].main;
+
+    let hi_low = document.querySelector('.hi-low');
+    hi_low.innerText = `${weather.main.temp_min}°c / ${weather.main.temp_max}°c`;
+}
+
+function dateBuilder(d) {
+    let months = ["January", "February", "March", "April", "May", "June", "July", 
+    "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", 
+    "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+}
